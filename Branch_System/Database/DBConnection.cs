8ff84d6251045244856de408b6adaa256e83c0bb
mtellesy <mtellesy@gmail.com>
2019-02-04 12:28:14 +0200

    Connecting to Remote DB

--- Branch_System/Database/DBConnection.cs
+++ Branch_System/Database/DBConnection.cs
@@ -9,7 +9,11 @@ namespace CTS.Database
 {
     public static class DBConnection
     {
-        private static string ConnectionString = @"Data Source=DESKTOP-9T6FOLM\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
+        //WIN-IKQ69RO950I\Administrator
+        //10.128.130.119,8888
+       // private static string ConnectionString = @"Data Source=DESKTOP-9T6FOLM\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
+        private static string ConnectionString = 
+            @"Server=10.128.130.119,8888; Database=CTS; User Id= sa; Password = TellesyM1195; ";
 
         public static SqlConnection Connection()
         {
