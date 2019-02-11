491ca3b410fba1b8333adf715125c799094c7c14
mtellesy <mtellesy@gmail.com>
2019-02-11 23:41:23 +0200

    #Start Working on Reports
    Download RDLC package
    Just started working on RechargeReports

---
+++ Branch_System/Screens/Reports/RechargeReports.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTS.Screens.Reports
+{
+    public partial class RechargeReports : Form
+    {
+        public RechargeReports()
+        {
+            InitializeComponent();
+        }
+    }
+}
