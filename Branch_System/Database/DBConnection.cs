ee2a4c7be99b2dd6b60319ef74debac0a0fb3820
mtellesy <mtellesy@gmail.com>
2019-02-11 18:25:56 +0200

    Edit for Desktop Enviroment

--- Branch_System/Database/DBConnection.cs
+++ Branch_System/Database/DBConnection.cs
@@ -10,10 +10,16 @@ namespace CTS.Database
     public static class DBConnection
     {
         //Local DB
-        private static string ConnectionString = @"Data Source=DESKTOP-9T6FOLM\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
+        // private static string ConnectionString = @"Data Source=DESKTOP-9T6FOLM\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
 
-         // Production DB
-       //private static string ConnectionString =  @"Server=10.128.130.119,8888; Database=CTS; User Id= sa; Password = TellesyM1195; ";
+        //Local DB Desktop
+        //private static string ConnectionString = @"Data Source=DESKTOP-77BFLLF\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
+
+        //All local Enviroment :: Should enable port first
+        private static string ConnectionString = @"Server=127.0.0.1,8888; Database=newDB; User Id= sa; Password = TellesyM1195;";
+
+        // Production DB
+        //private static string ConnectionString =  @"Server=10.128.130.119,8888; Database=CTS; User Id= sa; Password = TellesyM1195; ";
         // Test DB
         //private static string ConnectionString = @"Server=10.128.130.119,8888; Database=CTS-Test; User Id= sa; Password = TellesyM1195; ";
 
