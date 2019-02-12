f882848738fc695a9f733b3a92e07151430b4f65
mtellesy <mtellesy@gmail.com>
2019-02-12 13:40:09 +0200

    Start Working On ReportViewer

--- Branch_System/Database/DBConnection.cs
+++ Branch_System/Database/DBConnection.cs
@@ -9,14 +9,14 @@ namespace CTS.Database
 {
     public static class DBConnection
     {
-        //Local DB
-        // private static string ConnectionString = @"Data Source=DESKTOP-9T6FOLM\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
+        //Local DB Laptop
+         private static string ConnectionString = @"Data Source=DESKTOP-9T6FOLM\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
 
         //Local DB Desktop
         //private static string ConnectionString = @"Data Source=DESKTOP-77BFLLF\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
 
         //All local Enviroment :: Should enable port first
-        private static string ConnectionString = @"Server=127.0.0.1,8888; Database=newDB; User Id= sa; Password = TellesyM1195;";
+       // private static string ConnectionString = @"Server=127.0.0.1,8888; Database=newDB; User Id= sa; Password = TellesyM1195;";
 
         // Production DB
         //private static string ConnectionString =  @"Server=10.128.130.119,8888; Database=CTS; User Id= sa; Password = TellesyM1195; ";
