48ae7306a45dc486cd35adbafed9ce9332ef3afc
mtellesy <mtellesy@gmail.com>
2019-02-18 09:27:46 +0200

    Change Version to V1.1

--- Branch_System/Database/DBConnection.cs
+++ Branch_System/Database/DBConnection.cs
@@ -16,10 +16,10 @@ namespace CTS.Database
        // private static string ConnectionString = @"Data Source=DESKTOP-77BFLLF\SQLEXPRESS;Initial Catalog=newDB;Integrated Security=True";
 
         //All local Enviroment :: Should enable port first
-        private static string ConnectionString = @"Server=127.0.0.1,8888; Database=newDB; User Id= sa; Password = TellesyM1195;";
+        //private static string ConnectionString = @"Server=127.0.0.1,8888; Database=newDB; User Id= sa; Password = TellesyM1195;";
 
         // Production DB
-        //private static string ConnectionString =  @"Server=10.128.130.119,8888; Database=CTS; User Id= sa; Password = TellesyM1195; ";
+        private static string ConnectionString =  @"Server=10.128.130.119,8888; Database=CTS; User Id= sa; Password = TellesyM1195; ";
         // Test DB
         //private static string ConnectionString = @"Server=10.128.130.119,8888; Database=CTS-Test; User Id= sa; Password = TellesyM1195; ";
 
