8065cb42f2c8c2ca19ec0ba770cceb14f46b3068
mtellesy <mtellesy@gmail.com>
2020-01-01 11:54:15 +0200

    Start the Version 3.0.0
    #Working on local cards

---
+++ Branch_System/Screens/Main/Local Cards/Local_Inputter.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace CTS.Screens.Main.Local_Cards
+{
+    partial class Local_Inputter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Local_Inputter));
+            this.ShareFolder_BTN = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Reports_BTN = new System.Windows.Forms.Button();
+            this.PIN_BTN = new System.Windows.Forms.Button();
+            this.Reissue_BTN = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Recharge_BTN = new System.Windows.Forms.Button();
+            this.Issue_BTN = new System.Windows.Forms.Button();
+            this.Name_LBL = new System.Windows.Forms.Label();
+            this.Password_LBL = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Logout_BTN = new System.Windows.Forms.Button();
+            this.Branch_LBL = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Status_LBL = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ShareFolder_BTN
+            // 
+            this.ShareFolder_BTN.Font = new System.Drawing.Font("Modern No. 20", 10.25F);
+            this.ShareFolder_BTN.Location = new System.Drawing.Point(215, 360);
+            this.ShareFolder_BTN.Name = "ShareFolder_BTN";
+            this.ShareFolder_BTN.Size = new System.Drawing.Size(198, 61);
+            this.ShareFolder_BTN.TabIndex = 55;
+            this.ShareFolder_BTN.Text = "Shared Folder";
+            this.ShareFolder_BTN.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label6.Location = new System.Drawing.Point(12, 505);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 17);
+            this.label6.TabIndex = 53;
+            this.label6.Text = "Mu Tellesy Software ©";
+            // 
+            // Reports_BTN
+            // 
+            this.Reports_BTN.Font = new System.Drawing.Font("Modern No. 20", 16.25F);
+            this.Reports_BTN.Image = ((System.Drawing.Image)(resources.GetObject("Reports_BTN.Image")));
+            this.Reports_BTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Reports_BTN.Location = new System.Drawing.Point(6, 360);
+            this.Reports_BTN.Name = "Reports_BTN";
+            this.Reports_BTN.Size = new System.Drawing.Size(198, 61);
+            this.Reports_BTN.TabIndex = 52;
+            this.Reports_BTN.Text = "تقرير عمليات الشحن";
+            this.Reports_BTN.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Reports_BTN.UseVisualStyleBackColor = true;
+            // 
+            // PIN_BTN
+            // 
+            this.PIN_BTN.Enabled = false;
+            this.PIN_BTN.Font = new System.Drawing.Font("Modern No. 20", 16.25F);
+            this.PIN_BTN.Image = ((System.Drawing.Image)(resources.GetObject("PIN_BTN.Image")));
+            this.PIN_BTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PIN_BTN.Location = new System.Drawing.Point(215, 292);
+            this.PIN_BTN.Name = "PIN_BTN";
+            this.PIN_BTN.Size = new System.Drawing.Size(198, 62);
+            this.PIN_BTN.TabIndex = 47;
+            this.PIN_BTN.Text = "إصدار رقم سري";
+            this.PIN_BTN.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.PIN_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Reissue_BTN
+            // 
+            this.Reissue_BTN.Enabled = false;
+            this.Reissue_BTN.Font = new System.Drawing.Font("Modern No. 20", 18.55F);
+            this.Reissue_BTN.Image = ((System.Drawing.Image)(resources.GetObject("Reissue_BTN.Image")));
+            this.Reissue_BTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Reissue_BTN.Location = new System.Drawing.Point(215, 222);
+            this.Reissue_BTN.Name = "Reissue_BTN";
+            this.Reissue_BTN.Size = new System.Drawing.Size(198, 63);
+            this.Reissue_BTN.TabIndex = 46;
+            this.Reissue_BTN.Text = "إعادة إصدار";
+            this.Reissue_BTN.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Reissue_BTN.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(205, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "حالة الشحن و الإصدار";
+            // 
+            // Recharge_BTN
+            // 
+            this.Recharge_BTN.Enabled = false;
+            this.Recharge_BTN.Font = new System.Drawing.Font("Modern No. 20", 18.55F);
+            this.Recharge_BTN.Image = ((System.Drawing.Image)(resources.GetObject("Recharge_BTN.Image")));
+            this.Recharge_BTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Recharge_BTN.Location = new System.Drawing.Point(6, 291);
+            this.Recharge_BTN.Name = "Recharge_BTN";
+            this.Recharge_BTN.Size = new System.Drawing.Size(198, 63);
+            this.Recharge_BTN.TabIndex = 45;
+            this.Recharge_BTN.Text = "شحن";
+            this.Recharge_BTN.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Recharge_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Issue_BTN
+            // 
+            this.Issue_BTN.Enabled = false;
+            this.Issue_BTN.Font = new System.Drawing.Font("Modern No. 20", 18.55F);
+            this.Issue_BTN.Image = ((System.Drawing.Image)(resources.GetObject("Issue_BTN.Image")));
+            this.Issue_BTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Issue_BTN.Location = new System.Drawing.Point(6, 222);
+            this.Issue_BTN.Name = "Issue_BTN";
+            this.Issue_BTN.Size = new System.Drawing.Size(198, 63);
+            this.Issue_BTN.TabIndex = 44;
+            this.Issue_BTN.Text = "إصدار";
+            this.Issue_BTN.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Issue_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Name_LBL
+            // 
+            this.Name_LBL.AutoSize = true;
+            this.Name_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Name_LBL.Location = new System.Drawing.Point(61, 33);
+            this.Name_LBL.Name = "Name_LBL";
+            this.Name_LBL.Size = new System.Drawing.Size(20, 20);
+            this.Name_LBL.TabIndex = 40;
+            this.Name_LBL.Text = "X";
+            // 
+            // Password_LBL
+            // 
+            this.Password_LBL.AutoSize = true;
+            this.Password_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Underline);
+            this.Password_LBL.ForeColor = System.Drawing.Color.Blue;
+            this.Password_LBL.Location = new System.Drawing.Point(305, 9);
+            this.Password_LBL.Name = "Password_LBL";
+            this.Password_LBL.Size = new System.Drawing.Size(104, 20);
+            this.Password_LBL.TabIndex = 51;
+            this.Password_LBL.Text = "تغير كلمة المرور";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label1.Location = new System.Drawing.Point(363, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "مرحباً ";
+            // 
+            // Logout_BTN
+            // 
+            this.Logout_BTN.Font = new System.Drawing.Font("Modern No. 20", 16.25F);
+            this.Logout_BTN.Image = ((System.Drawing.Image)(resources.GetObject("Logout_BTN.Image")));
+            this.Logout_BTN.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Logout_BTN.Location = new System.Drawing.Point(215, 427);
+            this.Logout_BTN.Name = "Logout_BTN";
+            this.Logout_BTN.Size = new System.Drawing.Size(198, 61);
+            this.Logout_BTN.TabIndex = 48;
+            this.Logout_BTN.Text = "تسجيل الخروج";
+            this.Logout_BTN.UseVisualStyleBackColor = true;
+            // 
+            // Branch_LBL
+            // 
+            this.Branch_LBL.AutoSize = true;
+            this.Branch_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Branch_LBL.Location = new System.Drawing.Point(61, 57);
+            this.Branch_LBL.Name = "Branch_LBL";
+            this.Branch_LBL.Size = new System.Drawing.Size(20, 20);
+            this.Branch_LBL.TabIndex = 42;
+            this.Branch_LBL.Text = "X";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Status_LBL);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(59, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 47);
+            this.panel1.TabIndex = 41;
+            // 
+            // Status_LBL
+            // 
+            this.Status_LBL.AutoSize = true;
+            this.Status_LBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Status_LBL.Location = new System.Drawing.Point(10, 11);
+            this.Status_LBL.Name = "Status_LBL";
+            this.Status_LBL.Size = new System.Drawing.Size(20, 20);
+            this.Status_LBL.TabIndex = 4;
+            this.Status_LBL.Text = "X";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label7.Location = new System.Drawing.Point(347, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "رقم الفرع";
+            // 
+            // Local_Inputter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 538);
+            this.Controls.Add(this.ShareFolder_BTN);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Reports_BTN);
+            this.Controls.Add(this.PIN_BTN);
+            this.Controls.Add(this.Reissue_BTN);
+            this.Controls.Add(this.Recharge_BTN);
+            this.Controls.Add(this.Issue_BTN);
+            this.Controls.Add(this.Name_LBL);
+            this.Controls.Add(this.Password_LBL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Logout_BTN);
+            this.Controls.Add(this.Branch_LBL);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label7);
+            this.Name = "Local_Inputter";
+            this.Text = "Local_Inputter";
+            this.Load += new System.EventHandler(this.Local_Inputter_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ShareFolder_BTN;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Reports_BTN;
+        private System.Windows.Forms.Button PIN_BTN;
+        private System.Windows.Forms.Button Reissue_BTN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Recharge_BTN;
+        private System.Windows.Forms.Button Issue_BTN;
+        private System.Windows.Forms.Label Name_LBL;
+        private System.Windows.Forms.Label Password_LBL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Logout_BTN;
+        private System.Windows.Forms.Label Branch_LBL;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Status_LBL;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ Branch_System/Screens/Main/Local Cards/Local_Inputter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTS.Screens.Main.Local_Cards
+{
+    public partial class Local_Inputter : Form
+    {
+        public Local_Inputter()
+        {
+            InitializeComponent();
+        }
+
+        private void Local_Inputter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
