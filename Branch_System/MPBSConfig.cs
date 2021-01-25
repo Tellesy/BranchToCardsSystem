d18bf0ad0341711a17c2f8af91a7a507ce3efd55
mtellesy <mtellesy@gmail.com>
2021-01-25 23:08:49 +0200

    MPBS V 3.4.0

--- Branch_System/MPBSConfig.cs
+++ Branch_System/MPBSConfig.cs
@@ -16,6 +16,8 @@ namespace MPBS
         public static string PTSApplicationFiles = String.Format(PTSLocation + "PTSApplication");
         public static string PTSLoadFiles = String.Format(PTSLocation + "PTSLoad");
         public static string PINFiles = String.Format(PTSLocation + "PIN");
+        public static string SettlementFiles = String.Format(PTSLocation + "Settlement Files");
+
 
         public static void CreateFolders()
         {
@@ -25,6 +27,9 @@ namespace MPBS
             System.IO.Directory.CreateDirectory(PTSApplicationFiles);
             System.IO.Directory.CreateDirectory(PTSLoadFiles);
             System.IO.Directory.CreateDirectory(PINFiles);
+            System.IO.Directory.CreateDirectory(SettlementFiles);
+
+
         }
 
     }
